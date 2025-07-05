3a011bc757df342dbd976b87c7f1632b2a2e27c2
Tobias Queluz <tobiasq2001@hotmail.com>
2025-07-05 14:32:19 -0300

    Add commands to reload workspace

--- update.sh
+++ update.sh
@@ -1,2 +1,10 @@
 # Copy over myhyprland configs
 cp -R ~/.local/share/myhyprland/config/* ~/.config/
+
+# Touch alacritty config to pickup the changed theme
+touch "$HOME/.config/alacritty/alacritty.toml"
+
+# Restart for new theme
+pkill -SIGUSR2 waybar
+makoctl reload
+hyprctl reload
