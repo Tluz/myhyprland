0aa63280bc5c4b0329e5da50ed00502a06e7545e
Tobias Queluz <tobias.queluz@grupoabl.com.br>
2025-07-07 12:58:13 -0300

    Ajust waybar cpu module

--- update-config.sh
+++ update-config.sh
@@ -1,31 +1,8 @@
-#bash
-echo "./default/bash/rc" >~/.bashrc
-
 # Copy over myhyprland configs
 cp -R ./config/* ~/.config/
 
 # Touch alacritty config to pickup the changed theme
 touch "$HOME/.config/alacritty/alacritty.toml"
-
-gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-
-# Setup theme links
-mkdir -p ~/.config/myhyprland/themes
-for f in ./themes/*; do ln -s "$f" ~/.config/myhyprland/themes/; done
-
-# Set initial theme
-mkdir -p ~/.config/myhyprland/current
-ln -snf ~/.config/myhyprland/themes/tokyo-night ~/.config/myhyprland/current/theme
-
-# Set specific app links for current theme
-ln -snf ~/.config/myhyprland/current/theme/hyprlock.conf ~/.config/hypr/hyprlock.conf
-ln -snf ~/.config/myhyprland/current/theme/wofi.css ~/.config/wofi/style.css
-ln -snf ~/.config/myhyprland/current/theme/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
-mkdir -p ~/.config/btop/themes
-ln -snf ~/.config/myhyprland/current/theme/btop.theme ~/.config/btop/themes/current.theme
-mkdir -p ~/.config/mako
-ln -snf ~/.config/myhyprland/current/theme/mako.ini ~/.config/mako/config
 
 # Restart for new theme
 pkill -SIGUSR2 waybar
