67540787ff100f736c69aefe6d9f07016ae2668c
Tobias Queluz <tobiasq2001@hotmail.com>
2025-07-08 08:21:53 -0300

    Add qutebrowser in the default installation

--- install/desktop.sh
+++ install/desktop.sh
@@ -2,5 +2,5 @@ yay -S --noconfirm --needed \
   brightnessctl playerctl pamixer pavucontrol wireplumber \
   wl-clip-persist clipse \
   nautilus sushi ffmpegthumbnailer gnome-calculator \
-  chromium mpv \
+  chromium qutebrowser mpv \
   evince imv
