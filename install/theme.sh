c6635796a23ab2576996f6594cd33c05c91cdea2
Tobias Queluz <tobiasq2001@hotmail.com>
2025-07-08 07:52:58 -0300

    Fix initial background image setting

--- install/theme.sh
+++ install/theme.sh
@@ -13,7 +13,7 @@ for f in ~/.local/share/myhyprland/themes/*; do ln -s "$f" ~/.config/myhyprland/
 
 # Set initial theme
 mkdir -p ~/.config/myhyprland/current
-ln -snf ~/.config/myhyprland/themes/tokyo-night ~/.config/myhyprland/current/theme
+ln -snf ~/.config/myhyprland/themes/catppuccin ~/.config/myhyprland/current/theme
 
 # Set specific app links for current theme
 ln -snf ~/.config/myhyprland/current/theme/hyprlock.conf ~/.config/hypr/hyprlock.conf
@@ -23,3 +23,8 @@ mkdir -p ~/.config/btop/themes
 ln -snf ~/.config/myhyprland/current/theme/btop.theme ~/.config/btop/themes/current.theme
 mkdir -p ~/.config/mako
 ln -snf ~/.config/myhyprland/current/theme/mako.ini ~/.config/mako/config
+
+# Set new background
+ln -nsf $(find "$HOME/.config/myhyprland/current/theme/backgrounds/" -type f | head -n 1) "$HOME/.config/myhyprland/current/background"
+pkill -x swaybg
+setsid swaybg -i "$HOME/.config/myhyprland/current/background" -m fill &
