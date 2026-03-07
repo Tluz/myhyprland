615e66fdee21318e41251b24a4a6cc24ad0db49d
Tobias Queluz <tobiasq2001@hotmail.com>
2026-03-07 11:05:38 -0300

    Set yazi as the default file explorer

--- install/desktop.sh
+++ install/desktop.sh
@@ -2,5 +2,6 @@ yay -S --noconfirm --needed \
   brightnessctl playerctl pamixer pavucontrol wireplumber impala \
   wl-clip-persist clipse \
   nautilus sushi ffmpegthumbnailer gnome-calculator \
+  yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick \
   chromium qutebrowser mpv \
   evince imv
